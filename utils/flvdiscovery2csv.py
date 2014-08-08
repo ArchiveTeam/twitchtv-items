d5978e3ea3c4686a5e34f73da934fc8eeed336af
Christopher Foo <chris.foo@gmail.com>
2014-08-08 12:31:47 -0400

    Add some scripts for analyzing discovery.

---
+++ utils/flvdiscovery2csv.py
@@ -0,0 +1,70 @@
+'''Convert discovery items to tracker items.
+
+Use Python 3.
+'''
+import argparse
+import glob
+import itertools
+import gzip
+import json
+import csv
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('directory')
+    arg_parser.add_argument('csv_file')
+    arg_parser.add_argument('type', choices=['flv', 'user', 'video'])
+    
+    args = arg_parser.parse_args()
+    filenames = itertools.chain(
+        glob.iglob(args.directory + '/*.gz'),
+        glob.iglob(args.directory + '/*/*.gz'),
+    )
+
+    with open(args.csv_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        if args.type == 'flv':
+            writer.writerow(['video_id', 'index', 'url', 'type'])
+        elif args.type == 'user':
+            # is a plaintext file, no header needed
+            pass
+        elif args.type == 'video':
+            writer.writerow(['video_id', 'views'])
+        else:
+            raise Exception('Unsupported CSV type')
+    
+        for filename in filenames:
+            with gzip.GzipFile(filename, mode='rb') as in_file:
+                doc = json.loads(in_file.read().decode())
+            
+            if 'video_type' in doc and args.type == 'flv':
+                flv_file_discovery(doc, writer)
+            elif doc['type'] == 'discovery':
+                if args.type == 'user':
+                    user_discovery(doc, writer)
+                else:
+                    video_discovery(doc, writer)
+            else:
+                raise Exception('Unsupported discovery result')
+
+
+def flv_file_discovery(doc, writer):
+    for index in range(len(doc['urls'])):
+        url = doc['urls'][index]
+        writer.writerow([doc['id'], index, url, doc['video_type']])
+
+
+def user_discovery(doc, writer):
+    for user in users:
+        writer.writerow([user])
+
+
+def video_discovery(doc, writer):
+    for video_id, views in doc['videos']:
+        writer.writerow([video_id, views])
+    
+
+if __name__ == '__main__':
+    main()
---
+++ utils/flvdiscovery2items.py
@@ -0,0 +1,48 @@
+'''Convert discovery items to tracker items.
+
+Use Python 3.
+'''
+import argparse
+import glob
+import itertools
+import gzip
+import json
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('directory')
+    
+    args = arg_parser.parse_args()
+    filenames = itertools.chain(
+        glob.iglob(args.directory + '/*.gz'),
+        glob.iglob(args.directory + '/*/*.gz'),
+    )
+
+    for filename in filenames:
+        with gzip.GzipFile(filename, mode='rb') as in_file:
+            doc = json.loads(in_file.read().decode())
+        
+        if 'video_type' in doc:
+            flv_file_discovery(doc)
+        elif doc['type'] == 'discovery':
+            user_and_video_discovery(doc)
+        else:
+            raise Exception('Unknown discovery result')
+
+
+def flv_file_discovery(doc):
+    for url in doc['urls']:
+        print('url:{}'.format(url))
+
+
+def user_and_video_discovery(doc):
+    for user in doc['users']:
+        print('user:{}'.format(user))
+    
+    for video in doc['videos']:
+        print('flv:{}'.format(video))
+    
+
+if __name__ == '__main__':
+    main()
