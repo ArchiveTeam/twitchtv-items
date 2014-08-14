9f53c66f688649fca7d2c9a632989e23c1ea084d
David Yip <yipdw@member.fsf.org>
2014-08-14 10:37:35 -0500

    Don't generate ia upload lines for empty chunks.

--- utils/upload/gen-ia-upload.py
+++ utils/upload/gen-ia-upload.py
@@ -7,8 +7,9 @@ with open('serial') as f:
         serial = f.read()
 
 for dir in sys.argv[1:]:
-        last_component = dir.split('/')[-1]
-        item_name = "archiveteam_twitchtv_%s_%s" % (last_component, serial.strip())
-        title = 'Archive Team Twitch.tv: %s (%s)' % (last_component, serial.strip())
+        if len(os.listdir(dir)) > 0:
+            last_component = dir.split('/')[-1]
+            item_name = "archiveteam_twitchtv_%s_%s" % (last_component, serial.strip())
+            title = 'Archive Team Twitch.tv: %s (%s)' % (last_component, serial.strip())
 
-        print '/home/archiveteam/.local/bin/ia upload %s %s/*.warc.gz --metadata="mediatype:web" --metadata="title:%s" --metadata="date:2014" --metadata="language:eng" --metadata="collection:%s" --delete' % (item_name, dir, title, collection)
+            print '/home/archiveteam/.local/bin/ia upload %s %s/*.warc.gz -c --metadata="mediatype:web" --metadata="title:%s" --metadata="date:2014" --metadata="language:eng" --metadata="collection:%s" --delete' % (item_name, dir, title, collection)
